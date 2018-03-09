a4d55682cc5b5ff4194e7398198f7168e9b49151
Evgenii Grigorev <grigorev.evgenii@gmail.com>
2018-03-09 09:16:21 +0300

    Graphs

--- 5_decision_tree.py
+++ 5_decision_tree.py
@@ -26,20 +26,37 @@
 # 2. энтропия I_h
 # 3. ошибка классификации I_e
 
-import matplotlib. pyplot import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+
 def gini(p):
     return (p)*(1-(p))+(1-p)*(1-(1-p))
 
 def entropy(p):
     return -p*np.log2(p) - (1-p)*np.log2(1-p)
 
-as plt
-+ ( 1 - p ) * ( l -
-- ( 1 -     * n p . l o g 2 ( ( 1 -
-def error
-return 1 - np.max([p, 1 -
-  = np.arange      1.      1)
-ent=[entropy(p) if  !=  elseNone for  in   sc_ent =         if   else None for   in ent]
-err= [error(i)foriin
+def error(p):
+    return 1 - np.max([p, 1-p])
+
+x = np.arange(0.0, 1.0, 0.01)
+ent = [entropy(p) if p != 0 else None for p in x]
+sc_ent = [e * 0.5 if e else None for e in ent]
+err = [error(i) for i in x]
+
 fig =plt.figure()
-     plt.subplot(lll)
+ax = plt.subplot(111)
+
+for i , lab, ls, c, in zip([ent, sc_ent, gini(x), err],
+                    ['Энтропия', 'Энтропия (шкалированная)',
+                    'Неоднородность Джини', 'Ошибка классификации'],
+                    ['-', '-', '--', '-.'],
+                    ['black', 'lightgray', 'red', 'green', 'cyan']):
+    line = ax.plot(x, i, label = lab, linestyle = ls, lw = 2, color = c)
+ax.legend(loc='center', bbox_to_anchor=(0.5, 1.15),
+          ncol=3, fancybox=True, shadow=False)
+ax.axhline(y=0.5, linewidth=1 , color= 'k', linestyle= '--')
+ax.axhline(y=1.0, linewidth=1 , color= 'k', linestyle= '--')
+plt.ylim([0.0, 1.1])
+plt.xlabel('p(i=1)')
+plt.ylabel('Индекс неоднородности')
+plt.show()
