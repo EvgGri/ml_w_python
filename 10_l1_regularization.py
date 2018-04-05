1e36ea966c063c00fda7c8602c6362d10c0a31ae
Evgenii Grigorev <grigorev.evgenii.work@gmail.com>
2018-04-05 14:00:03 +0300

    Find error

--- 10_l1_regularization.py
+++ 10_l1_regularization.py
@@ -22,7 +22,7 @@ df_wine.describe()
 df_wine.shape
 
 # Разбиение на train и test выборки
-X, y = df_wine.iloc[:,:13].values, df_wine['Метка класса'].values
+X, y = df_wine.iloc[:,:12].values, df_wine['Метка класса'].values
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
@@ -96,4 +96,37 @@ lr.intercept_
 # соответствующий признак в 13-мерном наборе данных сортов вин для вычисления чистого входа
 lr.coef_
 # Отмечаем для себя, что весовые векторы разрежены, т.е. имеют всего несколько ненулевых записей.
+
 # Построим график траекторий регуляризации, т.е.  весовых коэффициентов, разных признаков для разных сил регуляризации.
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = plt.subplot(111)
+colors = ['blue', 'green', 'red', 'cyan',
+          'magenta', 'yellow', 'black',
+          'pink', 'lightgreen', 'lightblue',
+          'gray', 'indigo', 'orange']
+weights, params = [], []
+for c in np.arange(-4, 6, dtype=float):
+    lr = LogisticRegression(penalty='l1',
+                            C = 10 ** c,
+                            random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10**c)
+weights = np.array(weights)
+for column, color in zip(range(weights.shape[1]), colors):
+    plt.plot(params, weights[:, column],
+             label=df_wine.columns[column+1],
+             color=color)
+plt.axhline(0, color='black', linestyle='--', linewidth=3)
+plt.xlim([10**(-5),10**5])
+plt.ylabel('весовой коэффициент')
+plt.xlabel('C')
+plt.xscale('log')
+plt.legend(loc='upper left')
+ax.legend(loc='upper center',
+          bbox_to_anchor=(1.38,1.03),
+          ncol=1, fancybox=True)
+plt.show()
+# Итоговый график говорит о том, что все веса признаков будут обнулены, если штрафовать модель сильным параметром регуляризации
+# C < 0.1 , где C - инверсия параметра регуляризации.
