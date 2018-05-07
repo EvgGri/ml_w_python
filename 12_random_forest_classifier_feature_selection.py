d31c3a26256523beefb50c895926e7c9900b69c1
Evgenii Grigorev <grigorev.evgenii.work@gmail.com>
2018-05-07 11:09:37 +0300

    Сравнение важности

--- 12_random_forest_classifier_feature_selection.py
+++ 12_random_forest_classifier_feature_selection.py
@@ -143,5 +143,46 @@ plt.show()
 
 # К примеру, можно установить порог в 0.15 для сведения набора данных к 3 наиболее важных признаков.
 
-X_selected = forest.transform(X_train_std, threshold=0.15)
-X_selected.shape
+# Create a selector object that will use the random forest classifier to identify
+# features that have an importance of more than 0.15
+sfm = SelectFromModel(clf, threshold=0.15)
+from sklearn.feature_selection import SelectFromModel
+sfm = SelectFromModel(forest, threshold=0.15)
+
+# Train the selector
+sfm.fit(X_train_std, y_train)
+
+# Print the names of the most important features
+for feature_list_index in sfm.get_support(indices=True):
+    print(feat_labels[feature_list_index])
+
+#Create A Data Subset With Only The Most Important Features
+
+# Transform the data to create a new dataset containing only the most important features
+# Note: We have to apply the transform to both the training X and test X data.
+X_important_train = sfm.transform(X_train_std)
+X_important_test = sfm.transform(X_test_std)
+
+
+# Train A New Random Forest Classifier Using Only Most Important Features
+# Create a new random forest classifier for the most important features
+forest_important = RandomForestClassifier(n_estimators=10000, random_state=0, n_jobs=-1)
+
+# Train the new classifier on the new dataset containing the most important features
+forest_important.fit(X_important_train, y_train)
+
+# Compare The Accuracy Of Our Full Feature Classifier To Our Limited Feature Classifier
+
+# Apply The Full Featured Classifier To The Test Data
+y_pred = forest.predict(X_test_std)
+
+from sklearn.metrics import accuracy_score
+# View The Accuracy Of Our Full Feature (4 Features) Model
+accuracy_score(y_test, y_pred)
+
+
+# Apply The Full Featured Classifier To The Test Data
+y_important_pred = forest_important.predict(X_important_test)
+
+# View The Accuracy Of Our Limited Feature (2 Features) Model
+accuracy_score(y_test, y_important_pred)
