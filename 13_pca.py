9e00b2c720a4a9163c7f3bb64317757354f030d0
Evgenii Grigorev <grigorev.evgenii.work@gmail.com>
2018-05-25 13:41:05 +0300

    PCA

---
+++ 13_pca.py
@@ -0,0 +1,113 @@
+# Разбивка на тренировочное и тестовое подмножество
+
+import pandas as pd
+import numpy as np
+
+from sklearn.datasets import load_wine
+data = load_wine()
+data.target[[10, 80, 140]]
+list(data.target_names)
+
+
+df_wine = pd.DataFrame(data.data)
+
+df_wine.columns=['Алкоголь','Яблочная кислота','Зола',
+                 'Щелочность золы','Магний','Всего фенола','Флаваноиды',
+                 'Фенолы нефлаваноидные','Проантоцианины','Интенсивность цвета',
+                 'Оттенок','OD280 / OD315 разбавленных вин','Пролин']
+df_wine.head()
+df_wine['Метка класса'] = pd.DataFrame(data.target)
+df_wine.describe()
+# Размер data frame'a
+df_wine.shape
+
+# Разбиение на train и test выборки
+X, y = df_wine.iloc[:,0:13].values, df_wine['Метка класса'].values
+
+# Альтернативная задача массива данных
+# X = df_wine.ix[:,['Алкоголь','Яблочная кислота','Зола',
+#                  'Щелочность золы','Магний','Всего фенола','Флаваноиды',
+#                  'Фенолы нефлаваноидные','Проантоцианины','Интенсивность цвета',
+#                  'Оттенок','OD280 / OD315 разбавленных вин','Пролин']]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+# Масштабирование признаков(нормирование, приведение к диапазону 0-1)
+from sklearn.preprocessing import MinMaxScaler
+mms = MinMaxScaler()
+X_train_norm = mms.fit_transform(X_train)
+X_test_norm = mms.transform(X_test)
+
+# С практической точки зрения лучше использовать стандартизацию признаков (приведеение к нормальному распределению с единичной дисперсией).
+# Причина в том, что многие линейные модели, такие, как логистическая регрессия и метод опорных векторов инициализируют веса нулями или
+# близкими к 0 значениями, но вид нормального распределения упрощает извлечение весов.
+
+# Кроме того, стандартизация содержит полезную информацию о выбросах и делает алгоритм менее чувствительным к выбросам, в отличие от
+# минимаксного масштабирования, которое шкалирует данные в ограниченном диапазоне значений.
+
+# стандартизация признаков из модуля preprocessing
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+# вычисление параметров распределения для train данных (стандартное отклонение и мат. ожидание)
+# для каждого набора признаков. После вызова trasform мы стандартизируем тестовые и тренинговые данные.
+# Для стандартизации тестового набора мы используем теже самые параметры, вычисленные для train набора.
+# Поэтому значения в тренировочном и тестовом наборе сопоставимы.
+sc.fit(X_train)
+X_train_std = sc.fit_transform(X_train)
+X_test_std = sc.transform(X_test)
+
+
+
+# -=-=-= Факторный анализ
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X ,y , classifier, resolution = 0.02):
+
+    # Задаем генератор маркеров и палитру
+    markers=('s', 'x', 'o', '^', 'v')
+    colors=['red', 'blue', 'lightgreen', 'gray', 'green', 'cyan']
+    cmap=ListedColormap(colors[:len(np.unique(y))])
+
+    # Вывести поверхность решений для первых двух компонент факторного анализа
+    x1_min, x1_max = X[:,0].min()-1, X[:,0].max()+1
+    x2_min, x2_max = X[:,1].min()-1, X[:,1].max()+1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    # Разобраться, как работает функция ravel
+    Z=classifier.predict(np.array([xx1.ravel(),xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx1.max())
+
+    # Вывод образцов классов
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y== cl,0], y=X[y== cl,1], alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+
+pca = PCA(n_components=2)
+lr = LogisticRegression()
+
+pca = pca.fit(X_train_std)
+X_train_pca = pca.transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+
+lr = lr.fit(X_train_pca, y_train)
+
+# Выводим результаты для обучающего набора
+plot_decision_regions(X_train_pca, y_train, classifier=lr)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend(loc='lower left')
+plt.show()
+
+# Выводим результаты для тестового набора
+plot_decision_regions(X_test_pca, y_test, classifier=lr)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend(loc='lower left')
+plt.show()
