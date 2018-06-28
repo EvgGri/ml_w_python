dde63be88718c4229bd602a0e728885f167f8274
Evgenii Grigorev <grigorev.evgenii.work@gmail.com>
2018-06-28 16:15:21 +0300

    Error

--- 22_ensemble_vote_majority_vote.py
+++ 22_ensemble_vote_majority_vote.py
@@ -14,3 +14,25 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
 # Натренируем три разных классификатора - классификатор на основе логистической регрессии, дерева решений и к-ближайших соседей и посмотрим
 # на их индивидуальные качественные характеристики путем 10-блочной перекрестной проверки на тренировочном наборе данных, перед тем, как
 # объединить их в ансамблевый классификатор.
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+import numpy as np
+
+clf1 = LogisticRegression(penalty='l2', C=0.001, random_state=0)
+clf2 = DecisionTreeClassifier(max_depth=1, criterion="entropy", random_state=0)
+clf3 = KNeighborsClassifier(n_neighbors=1, p=2, metric='minkowski')
+
+pipe1 = Pipeline([['sc', StandardScaler()], ['clf', clf1]])
+pipe3 = Pipeline([['sc', StandardScaler()], ['clf', clf3]])
+
+clf_labels = ['Логистическая регрессия', 'Дерево решений', 'К ближайших соседей']
+print('10-блочная перекрестная проверка:\n')
+
+for clf, label in zip([pipe1,clf2, pipe3], clf_labels):
+    scores = cross_val_score(estimator='clf', X=X_train, y=y_train, cv=10, scoring='roc_auc')
+    print('ROC/AUC: 0.2f (+/- 0.2f) [%s]' % (scores.mean(), scores.std(), label))
+
+print('* ROC AUC = площадь под ROC-кривой')
