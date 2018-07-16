92d1e579c9f67181522ab50b50ebba65716005ca
Evgenii Grigorev <grigorev.evgenii.work@gmail.com>
2018-07-16 15:32:59 +0300

    No error

--- 22_ensemble_vote_majority_vote.py
+++ 22_ensemble_vote_majority_vote.py
@@ -61,75 +61,75 @@ class MajorityVoteClassifier(BaseEstimator, ClassifierMixin):
     weighta - веса, важность классификаторов, по умолчанию None
     """
 
-def __init__(self, classifiers, vote='classlabel', weights = None):
-    self.classifiers = classifiers
-    self.named_classifiers = {key: value for key, value in _name_estimators(classifiers)}
-    self.vote = vote
-    self.weights = weights
+    def __init__(self, classifiers, vote='classlabel', weights = None):
+        self.classifiers = classifiers
+        self.named_classifiers = {key: value for key, value in _name_estimators(classifiers)}
+        self.vote = vote
+        self.weights = weights
 
-def fit(self, X, y):
-    """Выполнить подгонку классификаторов
-    Х - разреженная матрица с тренировочными образцами [n_samples, n_features]
-    y - вектор целевых меток классов [n_samples]
+    def fit(self, X, y):
+        """Выполнить подгонку классификаторов
+        Х - разреженная матрица с тренировочными образцами [n_samples, n_features]
+        y - вектор целевых меток классов [n_samples]
 
-    Возвращает объект self
-    """
-    # Использовать объект LabelEncoder, чтобы гарантировать, что
-    # метки классов начинаются с 0, что важно для
-    # вызова np.argmax в self.predict
+        Возвращает объект self
+        """
+        # Использовать объект LabelEncoder, чтобы гарантировать, что
+        # метки классов начинаются с 0, что важно для
+        # вызова np.argmax в self.predict
 
-    self.lablenc_ = LabelEncoder()
-    self.lablenc_.fit(y)
-    self.classes_ = self.lablenc_.classes_
-    self.classifiers_ = []
-    for clf in self.classifiers:
-        fitted_clf = clone(clf).fit(X, self.lablenc_.transform(y))
-        self.classifiers_.append(fitted_clf)
+        self.lablenc_ = LabelEncoder()
+        self.lablenc_.fit(y)
+        self.classes_ = self.lablenc_.classes_
+        self.classifiers_ = []
+        for clf in self.classifiers:
+            fitted_clf = clone(clf).fit(X, self.lablenc_.transform(y))
+            self.classifiers_.append(fitted_clf)
 
-    return self
+        return self
 
-def predict(self, X):
-    """Спрогнозировать метки классов для Х
-    Х - разреженная матрица с тренировочными образцами [n_samples, n_features]
+    def predict(self, X):
+        """Спрогнозировать метки классов для Х
+        Х - разреженная матрица с тренировочными образцами [n_samples, n_features]
 
-    Возвращает
-    maj_vote - массив [n_samples], спрогнозированные метки классов
-    """
-    if self.vote == 'probability':
-        maj_vote = np.argmax(self.predict_proba(X), axis = 1)
-    else: #Голосование 'classlabel'
+        Возвращает
+        maj_vote - массив [n_samples], спрогнозированные метки классов
+        """
+        if self.vote == 'probability':
+            maj_vote = np.argmax(self.predict_proba(X), axis = 1)
+        else: #Голосование 'classlabel'
 
-        #Аккумулировать результаты из вызовов clf.predict
-        predictions = np.asarray([clf.predict(X) for clf in self.classifiers_]).T
-        maj_vote = np.apply_along_axis(lambda x: np.argmax(np.bincount(x, weights = self.weights)), axis = 1, arr = predictions)
-    maj_vote = self.lablenc_.inverse_transform(maj_vote)
-    return maj_vote
+            #Аккумулировать результаты из вызовов clf.predict
+            predictions = np.asarray([clf.predict(X) for clf in self.classifiers_]).T
+            maj_vote = np.apply_along_axis(lambda x: np.argmax(np.bincount(x, weights = self.weights)), axis = 1, arr = predictions)
+        maj_vote = self.lablenc_.inverse_transform(maj_vote)
+        return maj_vote
 
-def predict_proba(self, X):
-    """Спрогнозировать вероятность класса для Х
+    def predict_proba(self, X):
+        """Спрогнозировать вероятность класса для Х
 
-    Параметры:
-    Х - тренировочные векторы, [n_samples, n_features]
+        Параметры:
+        Х - тренировочные векторы, [n_samples, n_features]
 
-    Возвращает:
-    avg_proba - взвешенная средняя вероятность для каждого класса в рассчете на образец, [n_samples, n_classes]
-    """
+        Возвращает:
+        avg_proba - взвешенная средняя вероятность для каждого класса в рассчете на образец, [n_samples, n_classes]
+        """
 
-    probas = np.asarray([clf.predict_proba(X) for clf in self.classifiers_])
-    avg_proba = np.avarage(probas, axis = 0, weights = self.weights)
+        probas = np.asarray([clf.predict_proba(X) for clf in self.classifiers_])
+        avg_proba = np.avarage(probas, axis = 0, weights = self.weights)
 
-    return avg_proba
+        return avg_proba
 
-def get_params(self, deep = True):
-    """Получить имена парметров классификатора для GridSearch"""
+    def get_params(self, deep = True):
+        """Получить имена парметров классификатора для GridSearch"""
 
-    if not deep:
-        return super(MajorityVoteClassifier, self).get_params(deep = False)
-    else:
-        out = self.named_classifiers.copy()
-        for name, step in six.iteritems(self.named_classifiers):
-            for key, value in six.iteritems(step.get_params(deep = True)):
-                out['%s %s' % (name, key)] = value
-        return out
+        if not deep:
+            return super(MajorityVoteClassifier, self).get_params(deep = False)
+        else:
+            out = self.named_classifiers.copy()
+            for name, step in six.iteritems(self.named_classifiers):
+                for key, value in six.iteritems(step.get_params(deep = True)):
+                    out['%s %s' % (name, key)] = value
+            return out
 
 mv_clf = MajorityVoteClassifier(classifiers=[pipe1,clf2, pipe3])
