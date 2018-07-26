305867f6083ba890fe4865c9d49143d6dc9aab9f
Evgenii Grigorev <grigorev.evgenii.work@gmail.com>
2018-07-26 13:56:01 +0300

    Error is near

--- 23_bagging_ensemble.py
+++ 23_bagging_ensemble.py
@@ -13,6 +13,8 @@ data = load_wine()
 data.target[[10, 80, 140]]
 list(data.target_names)
 
+# url='https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data'
+# df_wine=pd.read_csv(url, header=None)
 
 df_wine = pd.DataFrame(data.data)
 
@@ -62,9 +64,38 @@ tree_test = accuracy_score(y_test, y_test_pred)
 
 print('Верность дерева решений на тренировочном/тестовом наборах %.3f/%.3f' % (tree_train, tree_test))
 
+# Модель хорошо работает на тренировочном наборе данных, но на тестовом точность значительно ниже, что свидетельствует о переобучении.
 
-# Масштабирование признаков(нормирование, приведение к диапазону 0-1)
-from sklearn.preprocessing import MinMaxScaler
-mms = MinMaxScaler()
-X_train_norm = mms.fit_transform(X_train)
-X_test_norm = mms.transform(X_test)
+bag = bag.fit(X_train, y_train)
+y_train_pred = bag.predict(X_train)
+y_test_pred = bag.predict(X_test)
+
+bag_train = accuracy_score(y_train, y_train_pred)
+bag_test = accuracy_score(y_test, y_test_pred)
+
+print('Верность дерева решений на тренировочном/тестовом наборах %.3f/%.3f' % (bag_train, bag_test))
+
+# Визуализация на первых двух компонентах
+import matplotlib.pyplot as plt
+
+x_min = X_train[:,0].min()-1
+x_max = X_train[:,0].max()+1
+y_min = X_train[:,1].min()-1
+y_max = X_train[:,1].max()-1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
+f, axarr = plt.subplots(nrows=1, ncols=2, sharex='col', sharey='row', figsize=(8,3))
+
+for idx, clf, tt in zip([0,1], [tree, bag], ["Дерево решений","Бэггинг"]):
+    clf.fit(X_train, y_train)
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    axarr[idx].contourf(xx, yy, Z, alpha=0.3)
+    axarr[idx].scatter(X_train[y_train==0, 0], X_train[y_train==0, 1], c='blue', marker='^')
+    axarr[idx].scatter(X_train[y_train==1, 0], X_train[y_train==1, 1], c='red', marker='o')
+    axarr[idx].set_title(tt)
+
+axarr[0].set_ylabel('Главная компонента №1', fontsize=12)
+# axarr[1].set_ylabel('Главная компонента №2', fontsize=12)
+plt.text(11, -1, s='Главная компонента №2', ha='center', va='center', fontsize=12)
+plt.show()
