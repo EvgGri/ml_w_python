8aa239ae1aac24fb73502fd324028883a8204ef3
Evgenii Grigorev <grigorev.evgenii.work@gmail.com>
2018-07-30 13:57:20 +0300

    Регуляризация

--- 26_nlp_tf_idf.py
+++ 26_nlp_tf_idf.py
@@ -99,3 +99,46 @@ stop = stopwords.words('english')
 [w  for w in tokenizer_porter('a runner likes running and runs a lot')[-10:] if w not in stop]
 
 # -=-=-=-=-=-=-= Тренировка лонистической регрессионной модели для задачи классификации документов -=-=-=-=-=-=-=
+# Классификация по 2-м классам: положительные отзывы и отрицательные отзывы
+X_train=df.loc[:25000,'review'].values
+y_train=df.loc[:25000,'sentiment'].values
+X_test=df.loc[25000:,'review'].values
+y_test=df.loc[25000:,'sentiment'].values
+
+# Затем воспользуемся объектом сеточного поиска GridSearchCV, чтобы определить оптимальные параметры для модели на 5-ти блочной
+# стратифицированной перекрестной проверке
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Инициализируем объект GridSearchCV и его сетку параметров
+# В приведенном ниже примере мы заменили векторизатор частотностей CountVectorizer и преобразователь tf-idf'ов TfidfTransformer
+# на векторизатор TfidfVectorizer, который объединяет данные объекты.
+# Наша сетка параметров param_grid состояла из 2-х словарей параметров, в первом словаре мы использовали векторизатор TfidfVectorizer
+# с его настройками по умолчанию, чтобы вычислить tf-idf'ы, во втором словаре мы установили эти параметры use_idf=False,
+# smooth_idf=False, norm = None, чтобы натренировать модель на основе исходных частот терминов.
+# Кроме того, для самой модели логистического классификатора, мы натренировали модели с использованием l1 & l2 регуляризации и штрафного
+# параметра clf__penalty и сравнили разные силы регуляризации, задав параметр обратной регуляризации C.
+tfidf=TfidfVectorizer(strip_accents=None, lowercase=False, preprocessor=None)
+param_grid = [{'vect__ngram_range': [(1,1)],
+               'vect__stop_words': [stop,None],
+               'vect__tokenizer': [tokenizer,tokenizer_porter],
+               'clf__penalty': ['l1','l2'],
+               'clf__C': [1.0,10.0,100.0]},
+               {
+               'vect__ngram_range': [(1,1)],
+               'vect__stop_words': [stop,None],
+               'vect__tokenizer': [tokenizer,tokenizer_porter],
+               'vect__use_idf': [False],
+               'vect__norm': [None],
+               'clf__penalty': ['l1','l2'],
+               'clf__C': [1.0,10.0,100.0]
+               }
+               ]
+
+lr_tfidf=Pipeline([('vect', tfidf),
+                   ('clf',LogisticRegression(random_state=0))
+                  ])
+gs_lr_tfidf=GridSearchCV(lr_tfidf, param_grid, scoring='accuracy',cv=5, verbose=1, n_jobs=-1)
+gs_lr_tfidf.fit(X_train,y_train)
