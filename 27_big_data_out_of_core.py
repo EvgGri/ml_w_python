3ec9e68a0dc18be54e3d7b848463f31ba2749167
Evgenii Grigorev <grigorev.evgenii.work@gmail.com>
2018-07-31 10:50:04 +0300

    Batch init

--- 27_big_data_out_of_core.py
+++ 27_big_data_out_of_core.py
@@ -22,3 +22,17 @@ def tokenizer(text):
     text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-','')
     tokenized = [w for w in text.split() if w not in stop]
     return tokenized
+
+# Определим генераторную функцию stream_docs, которая считывает и выдает по одному документу за раз
+def stream_docs(path):
+    with open(path,'r',encoding='utf-8') as csv:
+        next(csv) # пропускаем заголовок
+        for line in csv:
+            text, label = line[:-3], int(line[-2])
+            yield text, label
+
+# Первый документ должен вернуть кортеж, состоящий из текста отзыва и соответствующей метки класса
+next(stream_docs(path='./data/movie_data.csv'))
+# Определем функцию get_minibatch, которая принимает поток документов из функции stream_docs и возвращает отдельно взятое число докуметов,
+# заданных в параметре size
+def get_minibatch(doc_stream, size):
