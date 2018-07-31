8db51c0a1df74f253c48c284ea898c875b7d0bfc
Evgenii Grigorev <grigorev.evgenii.work@gmail.com>
2018-07-31 15:02:16 +0300

    Проверка модели

--- 27_big_data_out_of_core.py
+++ 27_big_data_out_of_core.py
@@ -77,3 +77,8 @@ for _ in range(45):
     X_train=vect.transform(X_train)
     clf.partial_fit(X_train, y_train, classes=classes)
     pbar.update()
+
+# Проверим качество моделирования на последних 5000 документах
+X_test, y_test = get_minibatch(doc_stream, size=5000)
+X_test=vect.transform(X_test)
+print('Верность: %.3f' % clf.score(X_test, y_test))
