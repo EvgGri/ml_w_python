76c584752cd49da620d4867cd43bf1e3d24f20b5
Evgenii Grigorev <grigorev.evgenii.work@gmail.com>
2018-09-07 16:25:07 +0300

    Errors

--- movieclassifier/vectorizer.py
+++ movieclassifier/vectorizer.py
@@ -4,3 +4,6 @@ from sklearn.feature_extraction.text import HashingVectorizer
 import re
 import os
 import pickle
+
+cur_dir = os.path.dirname(__file__)
+stop = pickle.load(open(os.path.join(cur_dir, 'pkl_objects', 'stopwords.pkl'), 'rb')
