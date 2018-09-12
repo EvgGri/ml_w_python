6837806b9018aedfe9fc6f8374f59751db88a3fb
Evgenii Grigorev <grigorev.evgenii.work@gmail.com>
2018-09-12 16:33:34 +0300

    Разведочный анализ

--- 31_linear_regression.py
+++ 31_linear_regression.py
@@ -3,7 +3,24 @@ import pandas as pd
 url='./data/BostonHousing.xls'
 
 df=pd.read_excel(url, index_col=None)
-df.columns = ['CRIM','ZN','INDUS','CHAS','NOX','RM','AGE','DIS','RAD','TAX','PT','B','LSTAT','MV']
-
-
+df.columns = ['CRIM','ZN','INDUS','CHAS','NOX','RM','AGE','DIS','RAD','TAX','PTRATIO','B','LSTAT','MEDV']
 df.head()
+
+ # Exploratory data analysis, EDA -  Разведочный анализ
+ # Создадим матрицу точечных графиков:
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style='whitegrid', context='notebook')
+cols = ['LSTAT', 'INDUS', 'NOX', 'RM', 'MEDV']
+sns.pairplot(df[cols], size=2.5)
+plt.show()
+# Вернуть стандартные стили
+# sns.reset_orig()
+
+# Корреляционная матрица - это квадратная матрица, которая содержит линеные коэффициенты корреляции Пирсона, которые измеряют линейную
+# зависимость между парами признаков. Коэффициенты корреляции ограничены диапазоном [-1,1].
+import numpy as np
+cm = np.corrcoef(df[cols].values.T)
+sns.set(font_scale=1.5)
+hm=sns.heatmap(cm,cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size':15}, yticklabels=cols, xticklabels=cols)
+plt.show()
