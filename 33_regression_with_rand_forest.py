42d58baa09ba9ef34b901c9b4e7f5c46a5cb86ae
Evgenii Grigorev <grigorev.evgenii.work@gmail.com>
2018-09-14 16:25:29 +0300

    Regression tree

--- 33_regression_with_rand_forest.py
+++ 33_regression_with_rand_forest.py
@@ -9,3 +9,43 @@
 
 # При использовании дерева решений для регрессии мы заменим энтропию на MSE.
 # В контексте регрессии на основе дерева решений MSE часто упоминается, как внутриузловая дисперсия.
+
+# Набор данных, содержащий информацию о зданиях в пригороде Бостона.
+import pandas as pd
+url='./data/BostonHousing.xls'
+
+df=pd.read_excel(url, index_col=None)
+df.columns = ['CRIM','ZN','INDUS','CHAS','NOX','RM','AGE','DIS','RAD','TAX','PTRATIO','B','LSTAT','MEDV']
+df.head()
+
+
+from sklearn.tree import DecisionTreeRegressor
+
+X = df[['LSTAT']].values
+y = df['MEDV'].values
+
+tree = DecisionTreeRegressor(max_depth=3)
+tree.fit(X, y)
+sort_idx = X.flatten().argsort()
+
+# Реализация обычной регрессионной модели методом наименьших квадратов (OLS - Ordinary least squares)
+from sklearn.linear_model import LinearRegression
+slr = LinearRegression()
+
+
+slr.fit(X,y)
+print('Наклон: %.3f' % slr.coef_[0])
+print('Пересечение: %.3f' % slr.intercept_)
+
+def lin_regplot(X,y, model):
+    plt.scatter(X,y,c='blue')
+    plt.plot(X, model.predict(X), color='red')
+    return None
+
+import matplotlib.pyplot as plt
+lin_regplot(X,y,slr)
+
+lin_regplot(X[sort_idx], y[sort_idx], tree)
+plt.xlabel('log(% населения с более низким статусом [LSTAT])')
+plt.ylabel('$\sqrt{Цена в тыс. долл. [MEDV]}$')
+plt.show()
