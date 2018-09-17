27f161b599cd28815ddef89f0e711309539b20b8
Evgenii Grigorev <grigorev.evgenii.work@gmail.com>
2018-09-17 17:27:05 +0300

    R^2

--- 33_regression_with_rand_forest.py
+++ 33_regression_with_rand_forest.py
@@ -86,3 +86,15 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 print('\nMSE тренировка: %.3f, тестирование: %.3f' % (mean_squared_error(y_train, y_train_pred),mean_squared_error(y_test, y_test_pred)))
 print('\nR^2 тренировка: %.3f, тестирование: %.3f' % (r2_score(y_train, y_train_pred),r2_score(y_test, y_test_pred)))
+import matplotlib.pyplot as plt
+# Случайный лес демонстрируем переподгонку под тренировочные данные.
+plt.scatter(y_train_pred, y_train_pred - y_train, c='blue', marker='o', label='Тренировочные данные')
+plt.scatter(y_test_pred, y_test_pred - y_test, c='lightgreen', marker='s', label='Тестовые данные')
+plt.xlabel('Предсказанные значения')
+plt.ylabel('Остатки')
+plt.legend(loc='upper left')
+plt.hlines(y=0, xmin=-10, xmax=50, lw=2, color='red')
+plt.xlim([-10,50])
+plt.show()
+ # Видно, что модель работает гораздо лучше соответствует тренировочным данным, чем тестовым, т.к. мы видим выбросы вдоль оси У.
+ # Это показывает коэффициент детерминации R^2.
