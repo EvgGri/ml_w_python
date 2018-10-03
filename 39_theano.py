fc7c1197a6e5521c385aedaf1576211786a12a5e
Evgenii Grigorev <grigorev.evgenii.work@gmail.com>
2018-10-03 17:00:02 +0300

    Теано - сценарий

--- 39_theano.py
+++ 39_theano.py
@@ -12,3 +12,14 @@ net_input=theano.function(inputs=[w1, x1, w0], outputs=z1)
 
 # Исполнить
 print('Чистый вход: %.2f' % net_input(2.0, 1.0, 0.5))
+
+# Настройка для вычисления не на процессоре, а на gpu
+print(theano.config.floatX)
+theano.config.floatX = 'float32'
+print(theano.config.floatX)
+
+# Bash - команда
+# export THEANO_FLAGS=floatX=float32
+
+# Применить формат только к конкретному сценарию
+# THEANO_ FLAGS=floatX=float32 python your_script.py
